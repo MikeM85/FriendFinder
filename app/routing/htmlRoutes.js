c6c3d680f27db48a701add69b5c04279d2919e86
Mike <mikeinhistory@gmail.com>
2019-03-19 22:00:09 -0500

    first try, nothing works, pages display ok

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+// Pull in required dependencies
+var path = require('path');
+
+// Export HTML routes
+module.exports = function(app) {
+
+
+	// Home page
+	app.get('/home.html', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
+
+	// Survey page
+	app.get('/survey', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/survey.html'));
+	});
+};
