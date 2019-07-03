bf2898a48e9fc3e11a407e080d25258ed94d4202
Mike <mikeinhistory@gmail.com>
2019-07-02 21:16:23 -0500

    build

---
+++ client/build/precache-manifest.73b7a9a6e5072b75a6e85d6e8bd983c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e99c52f8b085728bfb19",
+    "url": "/static/css/main.b3440eba.chunk.css"
+  },
+  {
+    "revision": "e99c52f8b085728bfb19",
+    "url": "/static/js/main.d92a83b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dd46ae3f342cedff79db",
+    "url": "/static/js/2.c176c9ff.chunk.js"
+  },
+  {
+    "revision": "b5bd085125e927ff38152d5e345de841",
+    "url": "/static/media/editbutton.b5bd0851.svg"
+  },
+  {
+    "revision": "6c63a23f7b302a45fbcd634bb005a530",
+    "url": "/static/media/dashboard.6c63a23f.jpg"
+  },
+  {
+    "revision": "6c63a23f7b302a45fbcd634bb005a530",
+    "url": "/static/media/showcase.6c63a23f.jpg"
+  },
+  {
+    "revision": "b8f3c23b21863a47bf28679d9381ca1f",
+    "url": "/index.html"
+  }
+];
